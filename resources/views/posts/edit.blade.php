ad5ffe98752d68eda64b9f777024ad5517fc0510
hirol413 <petuo1616@gmail.com>
2023-11-14 10:35:58 +0900

    編集機能作成

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <h1>blog</h1>
+        <form action="/posts/{{$post->id}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" value="{{old('post.title') ?: $post->title}}">
+                <p class="title_error" style="color:red">{{ $errors->first('post.title')}}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <input type="text" name="post[body]" value="{{old('post.body') ?: $post->body}}">
+                <p class="body_error" style="color:red">{{ $errors->first('post.body')}}</p>
+            </div>
+            <input type="submit" value="保存" />
+        </form>
+        <div class='footer'>
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
